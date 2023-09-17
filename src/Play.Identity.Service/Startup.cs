ed7715d7803b0bc425fcd5ab3d7d88ba76587fe3
SonPham <phamquangson1994@gmail.com>
2023-09-17 20:08:05 +0700

    Switch to generic Health extension methods

--- src/Play.Identity.Service/Startup.cs
+++ src/Play.Identity.Service/Startup.cs
@@ -14,6 +14,7 @@ using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
+using Play.Common.HealthChecks;
 using Play.Common.MassTransit;
 using Play.Common.Settings;
 using Play.Identity.Service.Entities;
@@ -89,32 +90,7 @@ namespace Play.Identity.Service
             });
 
             services.AddHealthChecks()
-                .Add(new HealthCheckRegistration(
-                    //Add anyname you want
-                    "mongodb",
-
-                    // the second parameter that we're going to provide here is going to be what we call the factory that is going to
-                    // create our model to be health check instance.
-                    serviceProvider =>
-                    {
-                        var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
-                        return new MongoDbHealthCheck(mongoClient);
-                    },
-
-                    //The next parameter is going to be the failure status. So this is going to be a status that should be reported if the
-                    // healthcheck fails.
-                    HealthStatus.Unhealthy,
-
-                    //The next parameter we're going to identify here is what we call the tax. So this is a way for you to group a different
-                    //set of health check registrations or health checks into specific groups that you want to report in different ways. So
-                    //in our case, we're going to create a group that we're going to be calling it, the ready group. Because we're going to
-                    //have both live test and readiness checks. And we'll talk about those in a moment. But for now, let's just assign the
-                    // ready tag to this.
-                    new[] { "ready" },
-                    // we can specify time out. So this is how much time this health check is going to wait before giving up. It's try to
-                    // do its thing. And if it can't get a healthy status, it's going to just a timeout and produce unhealthy status.
-                    TimeSpan.FromSeconds(3)
-                ));
+                .AddMongoDb();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -157,26 +133,8 @@ namespace Play.Identity.Service
                 endpoints.MapRazorPages();
 
                 //endpoints.MapHealthChecks("/health");
-                
-                //we also have to create appropriate routes so that our clients can access both our ready and are liveness health checks.
-                //we defined a very simple health route. So now we're going to define two routes. Like I said, one route for the readiness check
-                //and one route for the liveness check. The readiness check is going to be the one that's going to go ahead and make sure that
-                //this microservice is actively ready to start serving requests. So even if they make a service is up and running, it doesn't mean 
-                //that it is ready. It needs to make sure that, in this case. a database is ready to go and maybe some other services. While 
-                //liveness test is just going to be for making sure that these microservice is alive
-                endpoints.MapHealthChecks("/health/ready", new HealthCheckOptions()
-                {
-                    // this "ready" value comes from "ready" above you defined in .AddHealthChecks().Add(new HealthCheckRegistration...
-                    Predicate = (check) => check.Tags.Contains("ready") 
-                });
 
-                //this is a liveness check, the liveness check is just verify if the service is alive or not
-                 endpoints.MapHealthChecks("/health/live", new HealthCheckOptions()
-                {
-                    // false means at this point, do not filter out every single health check registration that you have, and just let me know
-                    //if the service can respond to this. So that's the way to provide that predicate.
-                    Predicate = (check) => false
-                });
+                endpoints.MapPlayEconomyHealthCheck();
             });
         }
     }
