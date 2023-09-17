8c35e5ee8489262e7dd62573ca4f28dcb3c14102
SonPham <phamquangson1994@gmail.com>
2023-09-17 09:45:51 +0700

    Added basic healthcheck

--- src/Play.Identity.Service/Startup.cs
+++ src/Play.Identity.Service/Startup.cs
@@ -83,6 +83,8 @@ namespace Play.Identity.Service
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Play.Identity.Service", Version = "v1" });
             });
+
+            services.AddHealthChecks();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -123,6 +125,8 @@ namespace Play.Identity.Service
             {
                 endpoints.MapControllers();
                 endpoints.MapRazorPages();
+                
+                endpoints.MapHealthChecks("/health");
             });
         }
     }
