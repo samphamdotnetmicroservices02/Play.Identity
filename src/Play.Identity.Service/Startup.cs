6f16e59a1b9a21ccc9f1d9a188129acf5ac5dc25
Son Quang Pham (SM) <sonpham@deloitte.ca>
2023-09-29 18:00:52 +0700

    update

--- src/Play.Identity.Service/Startup.cs
+++ src/Play.Identity.Service/Startup.cs
@@ -81,42 +81,42 @@ namespace Play.Identity.Service
             services.AddHealthChecks()
                 .AddMongoDb();
 
-            /*
-            * https://learn.dotnetacademy.io/courses/take/net-microservices-cloud/lessons/38615232-forwarding-headers-to-the-identity-microservice
-            * Let's configure identity microservice, so it can handle HTTPS reuqests coming from the API Gateway. So when you run a service like
-            * the identity service behind the API gateway, there are a couple of headers that you need to make sure that are forwarded from the 
-            * Api Gateway all the way down to you service. If that doesn't happen, then the HTTPS communication is not going to work properly.
-            * let's make sure that we do that header forwarding.
-            */
-            services.Configure<ForwardedHeadersOptions>(options =>
-            {
-                /*
-                * after configure this and app.UseForwardedHeaders(); below. You can use https for Api gateway.
-                * these headers here identifies what is going to be the originating IP address of the client. So when the client like postman, FE calls the
-                * Api gateway, what is the IP address from that client. So that's going to come in this header. And the other header is going to be 
-                * ForwardedHeaders.XForwardedProto. And what that contains is just the protocol that was used at originally, either it was HTTP or HTTPS.
-                * But that's going to come in that other header.
-                */
-                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+            // /*
+            // * https://learn.dotnetacademy.io/courses/take/net-microservices-cloud/lessons/38615232-forwarding-headers-to-the-identity-microservice
+            // * Let's configure identity microservice, so it can handle HTTPS reuqests coming from the API Gateway. So when you run a service like
+            // * the identity service behind the API gateway, there are a couple of headers that you need to make sure that are forwarded from the 
+            // * Api Gateway all the way down to you service. If that doesn't happen, then the HTTPS communication is not going to work properly.
+            // * let's make sure that we do that header forwarding.
+            // */
+            // services.Configure<ForwardedHeadersOptions>(options =>
+            // {
+            //     /*
+            //     * after configure this and app.UseForwardedHeaders(); below. You can use https for Api gateway.
+            //     * these headers here identifies what is going to be the originating IP address of the client. So when the client like postman, FE calls the
+            //     * Api gateway, what is the IP address from that client. So that's going to come in this header. And the other header is going to be 
+            //     * ForwardedHeaders.XForwardedProto. And what that contains is just the protocol that was used at originally, either it was HTTP or HTTPS.
+            //     * But that's going to come in that other header.
+            //     */
+            //     options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
 
-                /*
-                * And the next thing we want to do here is to clean up a couple of things that by default are already populated that you don't want them 
-                * populated with default values.
-                */
-                options.KnownNetworks.Clear();
-                options.KnownProxies.Clear();
-            });
+            //     /*
+            //     * And the next thing we want to do here is to clean up a couple of things that by default are already populated that you don't want them 
+            //     * populated with default values.
+            //     */
+            //     options.KnownNetworks.Clear();
+            //     options.KnownProxies.Clear();
+            // });
         }
 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            /*
-            * https://learn.dotnetacademy.io/courses/take/net-microservices-cloud/lessons/38615232-forwarding-headers-to-the-identity-microservice
-            * this make sure that the ForwardedHeaders are used at in the request pipeline.
-            */
-            app.UseForwardedHeaders();
+            // /*
+            // * https://learn.dotnetacademy.io/courses/take/net-microservices-cloud/lessons/38615232-forwarding-headers-to-the-identity-microservice
+            // * this make sure that the ForwardedHeaders are used at in the request pipeline.
+            // */
+            // app.UseForwardedHeaders();
 
             if (env.IsDevelopment())
             {
@@ -131,7 +131,7 @@ namespace Play.Identity.Service
                 });
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.Use((context, next) =>
             {
@@ -183,30 +183,31 @@ namespace Play.Identity.Service
 
                 //This is required in the Docker environment since the strict Linus permissions you'll set there won't allow IdentityServer to crea keys in
                 //the default /keys directory
-                options.KeyManagement.KeyPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+                // options.KeyManagement.KeyPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
             })
             .AddAspNetIdentity<ApplicationUser>()
             .AddInMemoryApiScopes(identityServerSettings.ApiScopes)
             .AddInMemoryApiResources(identityServerSettings.ApiResources)
             .AddInMemoryClients(identityServerSettings.Clients)
+            .AddDeveloperSigningCredential()
             .AddInMemoryIdentityResources(identityServerSettings.IdentityResources);
 
-            //After configuring TLS certificate, this code base uses for singing key authen/author in production
-            if (!_environment.IsDevelopment())
-            {
-                var identitySettings = Configuration.GetSection(nameof(IdentitySettings)).Get<IdentitySettings>();
+            // //After configuring TLS certificate, this code base uses for singing key authen/author in production
+            // if (!_environment.IsDevelopment())
+            // {
+            //     var identitySettings = Configuration.GetSection(nameof(IdentitySettings)).Get<IdentitySettings>();
 
-                /*
-                * read the values of the certificate files in Kubernetes secret
-                * CreateFromPemFile(), remember that this file is being mounted into a directory in the microservice file system. It is being mounted by
-                * Kubernetes. So we want to read it from there.
-                */
-                var cer = X509Certificate2.CreateFromPemFile(
-                    identitySettings.CertificateCerFilePath,
-                    identitySettings.CertificateKeyFilePath
-                );
-                builder.AddSigningCredential(cer);
-            }
+            //     /*
+            //     * read the values of the certificate files in Kubernetes secret
+            //     * CreateFromPemFile(), remember that this file is being mounted into a directory in the microservice file system. It is being mounted by
+            //     * Kubernetes. So we want to read it from there.
+            //     */
+            //     var cer = X509Certificate2.CreateFromPemFile(
+            //         identitySettings.CertificateCerFilePath,
+            //         identitySettings.CertificateKeyFilePath
+            //     );
+            //     builder.AddSigningCredential(cer);
+            // }
         }
     }
 }
