0a7e9718211b0bae735701b814d68765fc1d2b12
SonPham <phamquangson1994@gmail.com>
2023-10-19 08:01:58 +0700

    Add the AddTracing extension method

--- src/Play.Identity.Service/Startup.cs
+++ src/Play.Identity.Service/Startup.cs
@@ -17,6 +17,7 @@ using MongoDB.Bson.Serialization.Serializers;
 using Play.Common.HealthChecks;
 using Play.Common.Logging;
 using Play.Common.MassTransit;
+using Play.Common.OpenTelemetry;
 using Play.Common.Settings;
 using Play.Identity.Service.Entities;
 using Play.Identity.Service.Exceptions;
@@ -78,7 +79,8 @@ namespace Play.Identity.Service
             services.AddHealthChecks()
                 .AddMongoDb();
             
-            services.AddSeqLogging(Configuration);
+            services.AddSeqLogging(Configuration)
+                .AddTracing(Configuration);
 
             // in local kubernetes, we don't use HTTPS
             if (!serviceSettings.IsKubernetesLocal.Equals("true", StringComparison.OrdinalIgnoreCase))
